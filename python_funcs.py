a0fc88a6a6017fbe75886521770452792e6b9653
jmichael3 <45578785+jmichael3@users.noreply.github.com>
2019-06-18 20:37:07 -0400

    added is_triangle
    added func that returns boolean based on if a, b, and c can form a triangle

--- python_funcs.py
+++ python_funcs.py
@@ -1,3 +1,11 @@
 def friend(x):
     '''takes a list of strings and returns strips the list of anything that isn't exactly 4 chars'''
     return [_ for _ in x if len(_) == 4]
+
+def is_triangle(a, b, c):
+    '''given a, b, and c returns boolean value based on if the values can form a valid 
+    triangle (Triangle Inequality Theorem) a + b > c; a + c > b; b + c > a'''
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    else:
+        return False
